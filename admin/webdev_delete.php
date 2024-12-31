94971ca9b4a49f1a5f8c286ca6bddf16d5bcaab5
Nirav kagathara <121386601+niravkagathara@users.noreply.github.com>
2024-12-31 21:40:12 +0530

    Delete admin directory

--- admin/webdev_delete.php
+++
@@ -1,7 +0,0 @@
-<?php 
-require_once 'auth_check.php';
-include_once '../classes/Webdevs.php';
-$dev = new Webdevs();
-$_SESSION['dele']=$dev->deleteById($_GET['id']);
-header('Location:webdev.php');
-?>
